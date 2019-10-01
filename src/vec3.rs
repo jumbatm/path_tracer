6665c3a8d4d963433dd25bda2e719f01aa6bde50
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-10-02 09:55:25 +1000

    Add vector dot and cross product.

--- src/vec3.rs
+++ src/vec3.rs
@@ -7,9 +7,37 @@ impl<T: Default> Default for Vec3<T> {
     }
 }
 
+impl<T: Copy> Copy for Vec3<T> {}
+
 impl<T> Vec3<T> {
     pub fn new(x: T, y: T, z: T) -> Self {
         Vec3(x, y, z)
+    }
+}
+
+impl<T: std::ops::Mul<Output = T> + std::ops::Add<Output = T>> Vec3<T> {
+    pub fn dot(self, rhs: Self) -> T {
+        self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2
+    }
+}
+
+impl<T> Vec3<T>
+where
+    T: std::ops::Mul<Output = T> + std::ops::Add<Output = T> + std::ops::Sub<Output = T> + Copy,
+{
+    pub fn cross(self, rhs: Self) -> Self {
+        Vec3(
+            self.1 * rhs.2 - self.2 * rhs.1,
+            self.2 * rhs.0 - self.0 * rhs.2,
+            self.0 * rhs.1 - self.1 * rhs.0,
+        )
+    }
+}
+
+impl<T: std::ops::Mul<Output = T> + Copy> std::ops::Mul<T> for Vec3<T> {
+    type Output = Self;
+    fn mul(self, rhs: T) -> Self {
+        Vec3(self.0 * rhs, self.1 * rhs, self.2 * rhs)
     }
 }
 
@@ -27,6 +55,13 @@ impl<T: std::ops::Sub<Output = T>> std::ops::Sub for Vec3<T> {
     }
 }
 
+impl<T: std::ops::Neg<Output = T>> std::ops::Neg for Vec3<T> {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        Vec3(-self.0, -self.1, -self.2)
+    }
+}
+
 impl<T: PartialEq> PartialEq for Vec3<T> {
     fn eq(&self, other: &Self) -> bool {
         self.0 == other.0 && self.1 == other.1 && self.2 == other.2
@@ -41,9 +76,33 @@ mod tests {
 
     #[test]
     fn test_add() {
-        let x = Vec3::new(1, 2, 3);
-        let y = Vec3::new(4, 3, 2);
+        let u = Vec3::new(1, 2, 3);
+        let v = Vec3::new(4, 3, 2);
 
-        assert_eq!(x + y, Vec3::new(5, 5, 5));
+        assert_eq!(u + v, Vec3::new(5, 5, 5));
+    }
+
+    #[test]
+    fn test_dot() {
+        let u = Vec3::new(1, 2, 3);
+        let v = Vec3::new(4, 5, 6);
+
+        assert_eq!(u.dot(v), 4 + 10 + 18);
+    }
+
+    #[test]
+    fn test_cross() {
+        let i = Vec3::new(1, 0, 0);
+        let j = Vec3::new(0, 1, 0);
+        let k = Vec3::new(0, 0, 1);
+
+        assert_eq!(i.cross(j), k);
+        assert_eq!(j.cross(i), -k);
+
+        assert_eq!(j.cross(k), i);
+        assert_eq!(k.cross(j), -i);
+
+        assert_eq!(k.cross(i), j);
+        assert_eq!(i.cross(k), -j);
     }
 }
