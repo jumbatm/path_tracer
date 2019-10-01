d592bcf686d66f988a6e72f29de1b68ac7cd257d
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-10-02 09:56:29 +1000

    Add getters for origin and direction. Remove polar constructor.

--- src/ray.rs
+++ src/ray.rs
@@ -9,13 +9,10 @@ impl Ray {
     fn new(origin: vec3::Vec3<f64>, direction: vec3::Vec3<f64>) -> Ray {
         Ray { origin, direction }
     }
-
-    fn new_from_polar(origin: vec3::Vec3<f64>, elevation: f64, azimuth: f64) -> Ray {
-        let direction = vec3::Vec3::new(
-            azimuth.cos() * elevation.sin(),
-            elevation.sin(),
-            azimuth.sin(),
-        );
-        Ray::new(origin, direction)
+    fn get_origin(&self) -> &vec3::Vec3<f64> {
+        &self.origin
+    }
+    fn get_direction(&self) -> &vec3::Vec3<f64> {
+        &self.direction
     }
 }
