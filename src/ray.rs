880cc6b3998b2b5714cdc9308fec1fe5367f7682
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-10-03 16:30:40 +1000

    Add Colour, ColouredRay

--- src/ray.rs
+++ src/ray.rs
@@ -1,18 +1,62 @@
 use crate::vec3;
 
-struct Ray {
+pub struct Ray {
     origin: vec3::Vec3<f64>,    // A point.
     direction: vec3::Vec3<f64>, // A unit vector pointing in the ray's direction.
 }
 
 impl Ray {
-    fn new(origin: vec3::Vec3<f64>, direction: vec3::Vec3<f64>) -> Ray {
+    pub fn new(origin: vec3::Vec3<f64>, direction: vec3::Vec3<f64>) -> Ray {
         Ray { origin, direction }
     }
-    fn get_origin(&self) -> &vec3::Vec3<f64> {
+    pub fn get_origin(&self) -> &vec3::Vec3<f64> {
         &self.origin
     }
-    fn get_direction(&self) -> &vec3::Vec3<f64> {
+    pub fn get_direction(&self) -> &vec3::Vec3<f64> {
         &self.direction
+    }
+}
+
+pub struct Colour(vec3::Vec3<u8>);
+impl Colour {
+    pub fn new(r: u8, g: u8, b: u8) -> Colour {
+        Colour(vec3::Vec3::new(r, g, b))
+    }
+    pub fn get_red(&self) -> u8 {
+        (self.0).0
+    }
+    pub fn get_green(&self) -> u8 {
+        (self.0).1
+    }
+    pub fn get_blue(&self) -> u8 {
+        (self.0).2
+    }
+    pub fn mut_red(&mut self) -> &mut u8 {
+        &mut (self.0).0
+    }
+    pub fn mut_green(&mut self) -> &mut u8 {
+        &mut (self.0).1
+    }
+    pub fn mut_blue(&mut self) -> &mut u8 {
+        &mut (self.0).2
+    }
+}
+
+pub struct ColouredRay(Colour, Ray);
+impl ColouredRay {
+    pub fn new(colour: Colour, ray: Ray) -> ColouredRay {
+        ColouredRay(colour, ray)
+    }
+    pub fn get_colour(&self) -> &Colour {
+        &self.0
+    }
+    pub fn get_ray(&self) -> &Ray {
+        &self.1
+    }
+    pub fn mut_colour(&mut self) -> &mut Colour {
+        &mut self.0
+    }
+    pub fn mut_ray(&mut self) -> &mut Ray {
+        &mut self.1
     }
 }
