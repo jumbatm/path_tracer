6138bb993abfc12ba27d85ae53c7c014e47b6b5e
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-10-20 17:44:21 +1000

    Make FFI use Scenes, make materials.

--- path_tracer_ffi/ffi/c/include/PT.h
+++ path_tracer_ffi/ffi/c/include/PT.h
@@ -5,10 +5,20 @@
  */
 #include <stdint.h>
 
+////////////////////////////////////////////////////////////////////////////////
+// Opaque types.
+////////////////////////////////////////////////////////////////////////////////
+
 struct Scene;
 struct Vec3;
 struct Camera;
 struct Image;
+struct Hit;
+struct Material;
+
+////////////////////////////////////////////////////////////////////////////////
+// The Pixel type. Used to represent the colour of pixels in a render.
+////////////////////////////////////////////////////////////////////////////////
 
 struct Pixel {
     uint8_t red;
@@ -20,6 +30,41 @@ struct Pixel {
 struct Vec3 *PT_Vec3_new(double x, double y, double z);
 void PT_Vec3_delete(struct Vec3 *vec);
 
+/// Create a Scene.
+struct Scene *PT_Scene_new();
+
+/// Add some object to a scene. The scene will take ownership of the object.
+void PT_Scene_add_object(struct Scene *self, struct Hit *object);
+
+////////////////////////////////////////////////////////////////////////////////
+// Materials.
+////////////////////////////////////////////////////////////////////////////////
+
+/// Destroy a material. This will not invalidate objects using this material -
+/// only invalidate using this material in any new objects.
+void PT_Material_delete(struct Material *handle);
+
+/// Create a Lambertian material.
+struct Material *PT_Material_Lambertian_new(double red, double green, double blue, float fuzziness);
+
+/// Create Debugon, a magical material which has no attenution and is coloured
+/// by its surface normal.
+struct Material *PT_Material_Debugon_new();
+
+////////////////////////////////////////////////////////////////////////////////
+// Shapes.
+////////////////////////////////////////////////////////////////////////////////
+
+/// Create a Sphere with a given material.
+struct Hit *PT_Sphere_new(double x, double y, double z, double radius, struct Material *material);
+/// Destroy a sphere handle. This will not invalidate any scenes using this
+/// sphere -- only adding this sphere to anymore scenes.
+struct Hit *PT_Sphere_delete(struct Sphere *sphere);
+
+////////////////////////////////////////////////////////////////////////////////
+// Camera.
+////////////////////////////////////////////////////////////////////////////////
+
 /// Create a camera for some scene. This function will take ownership of the
 /// scene.
 struct Camera *PT_Camera_new(struct Scene *scene, struct Vec3 *origin, struct Vec3 *up, struct Vec3 *forward, uint64_t x_size, uint64_t y_size);
@@ -29,13 +74,18 @@ struct Camera *PT_Camera_new(struct Scene *scene, struct Vec3 *origin, struct Ve
 /// rendered in one portion.
 struct Image *PT_Camera_render(struct Camera *self, uint64_t topleft_x, uint64_t topleft_y, uint64_t bottomright_x, uint64_t bottomright_y, double fov, uint64_t bounces, uint64_t samples_per_pixel);
 
+////////////////////////////////////////////////////////////////////////////////
+// Image.
+////////////////////////////////////////////////////////////////////////////////
+
 /// Free the image.
 void PT_Image_delete(struct Image *image);
 
-/// Once we have an Image, get the colour of a certain pixel of it. Returns
-/// black if out of bounds access is attempted.
-struct Pixel PT_Image_get_pixel(struct Image *image, uint64_t i, uint64_t j);
+/// Get a certain pixel's colour.
+struct Pixel PT_Image_get_pixel(struct Image *image, uint64_t i, uint64_t j); 
 
-uint64_t PT_Image_get_height(struct Image *image);
+/// Get the image's height.
+uint64_t PT_Image_get_height(struct Image *image); 
 
+/// Get the images width.
 uint64_t PT_Image_get_width(struct Image *image);
--- path_tracer_ffi/src/lib.rs
+++ path_tracer_ffi/src/lib.rs
@@ -1,17 +1,14 @@
-use path_tracer::camera;
-use path_tracer::image;
-use path_tracer::vec3;
-use path_tracer::sphere;
-use path_tracer::colour;
-
-use path_tracer::lambertian;
+use path_tracer::*;
 
 use std::convert::TryInto;
 
-pub type CCamera = camera::Camera<sphere::Sphere>;
+pub type CCamera = camera::Camera<scene::Scene<'static>>;
 pub type CVec3 = vec3::Vec3<f64>;
+pub type CMaterial = std::rc::Rc<dyn material::Material>;
+pub type CSphere = std::rc::Rc<sphere::Sphere>;
+pub type CHit = std::rc::Rc<dyn hit::Hit + 'static>;
 
-use std::os::raw::{c_char, c_double};
+use std::os::raw::{c_double, c_float};
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -64,8 +61,49 @@ pub unsafe extern "C" fn PT_Vec3_delete(vec: *mut CVec3) {
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn PT_Scene_new() -> *mut scene::Scene<'static> {
+    Box::into_raw(Box::new(scene::Scene::new()))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn PT_Scene_add_object(self_: *mut scene::Scene, object: *mut CHit) {
+    self_.as_mut().unwrap().add_object(object.as_ref().unwrap().clone());
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn PT_Material_Lambertian_new(red: c_double, green: c_double, blue: c_double, fuzziness: c_float) -> *mut CMaterial {
+    Box::into_raw(Box::new(std::rc::Rc::new(lambertian::Lambertian::new(colour::Colour::new(red, green, blue), fuzziness))))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn PT_Material_delete(material: *mut CMaterial) {
+    if !material.is_null() {
+        Box::from_raw(material);
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn PT_Material_Debugon_new() -> *mut CMaterial {
+    Box::into_raw(Box::new(std::rc::Rc::new(debugon::Debugon::new())))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn PT_Sphere_new(x: c_double, y: c_double, z: c_double, radius: c_double, material: *mut CMaterial) -> *mut CSphere {
+    let material_rc = std::rc::Rc::clone(material.as_ref().unwrap());
+
+    // Return our new sphere.
+    Box::into_raw(Box::new(std::rc::Rc::new(sphere::Sphere::new(WorldVec::new(x, y, z), radius, material_rc))))
+}
+#[no_mangle]
+pub unsafe extern "C" fn PT_Sphere_delete(sphere: *mut CSphere) {
+    if !sphere.is_null() {
+        Box::from_raw(sphere);
+    }
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn PT_Camera_new(
-    scene: *mut sphere::Sphere,
+    scene: *mut scene::Scene<'static>,
     origin: *mut CVec3,
     up: *mut CVec3,
     forward: *mut CVec3,
