c7114008d65e5c2805a4afcc4c86a89c620c0473
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-11-02 00:10:05 +1000

    Add a test checking we can render multiple images from the same scene.

--- path_tracer_ffi/tests/integration_tests.rs
+++ path_tracer_ffi/tests/integration_tests.rs
@@ -61,4 +61,45 @@ mod tests {
 
         }
     }
+
+    #[test]
+    fn can_render_multi() {
+        unsafe {
+            let scene = ffi::PT_SceneBuilder_new();
+            let material = ffi::PT_Material_Debugon_new();
+            let sphere = ffi::PT_Sphere_new(0.0, 0.0, 0.0, 1.0, material);
+            ffi::PT_SceneBuilder_add_object(scene, sphere);
+
+            let sphere = ffi::PT_Sphere_new(0.0, 0.0, 0.0, 1.0, material);
+            ffi::PT_SceneBuilder_add_object(scene, sphere);
+
+            let camera = ffi::PT_Camera_new(
+                ffi::PT_SceneBuilder_into_scene(scene),
+                /*origin=*/ ffi::PT_Vec3_new(0.0, 0.0, -1.0),
+                /*forward=*/ ffi::PT_Vec3_new(0.0, 1.0, 0.0),
+                /*up=*/ ffi::PT_Vec3_new(0.0, 0.0, 1.0),
+            );
+            // Top left.
+            let image1 = ffi::PT_Camera_render(camera, 0, 0, 100, 50, 200, 100, 90.0, 10, 10);
+            // Top right.
+            let image2 = ffi::PT_Camera_render(camera, 100, 0, 100, 50, 200, 100, 90.0, 10, 10);
+            // Bottom left.
+            let image3 = ffi::PT_Camera_render(camera, 0, 50, 100, 50, 200, 100, 90.0, 10, 10);
+            // Bottom right.
+            let image4 = ffi::PT_Camera_render(camera, 100, 50, 100, 50, 200, 100, 90.0, 10, 10);
+
+            assert_eq!(ffi::PT_Image_get_width(image1), 100);
+            assert_eq!(ffi::PT_Image_get_height(image1), 50);
+
+            assert_eq!(ffi::PT_Image_get_width(image2), 100);
+            assert_eq!(ffi::PT_Image_get_height(image2), 50);
+
+            assert_eq!(ffi::PT_Image_get_width(image3), 100);
+            assert_eq!(ffi::PT_Image_get_height(image3), 50);
+
+            assert_eq!(ffi::PT_Image_get_width(image4), 100);
+            assert_eq!(ffi::PT_Image_get_height(image4), 50);
+        }
+
+    }
 }
