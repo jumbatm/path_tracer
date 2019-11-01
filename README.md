77f96ab6b266319e06ad7f50dcbcda1625cb8dfd
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-11-02 03:03:42 +1000

    Add an example render and a README.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Path Tracer
+A path tracer implemented in Rust. Implemented using [Ray Tracing In a Weekend](https://raytracing.github.io) as a guide.
+
+# Example renders
+![](samples/three_materials.png)
--- path_tracer/src/main.rs
+++ path_tracer/src/main.rs
@@ -5,15 +5,20 @@ use path_tracer::sphere;
 use path_tracer::colour;
 use path_tracer::lambertian;
 use path_tracer::metal;
+use path_tracer::debugon;
 
 const TOP_SPHERE_RADIUS: f64 = 1.0;
 
 fn main() {
     let mut scene = scene::Scene::new();
 
-    let top_sphere1 = std::rc::Rc::new(sphere::Sphere::new(vec3::Vec3::new(-TOP_SPHERE_RADIUS, 0.0, 0.0), TOP_SPHERE_RADIUS, std::rc::Rc::new(lambertian::Lambertian::new(colour::Colour::new(0.5, 0.5, 0.5)))));
-    let top_sphere2 = std::rc::Rc::new(sphere::Sphere::new(vec3::Vec3::new(TOP_SPHERE_RADIUS, 0.0, 0.0), TOP_SPHERE_RADIUS, std::rc::Rc::new(metal::Metal::new(colour::Colour::new(0.5, 0.5, 0.5), 1.0))));
-    let bottom_sphere = std::rc::Rc::new(sphere::Sphere::new(vec3::Vec3::new(0.0, -100.0 - TOP_SPHERE_RADIUS, 0.0), 100.0, std::rc::Rc::new(lambertian::Lambertian::new(colour::Colour::new(0.5, 0.5, 0.5)))));
+    let metal = std::rc::Rc::new(metal::Metal::new(colour::Colour::new(0.5, 0.5, 0.5), 1.0));
+    let gray_lambertian = std::rc::Rc::new(lambertian::Lambertian::new(colour::Colour::new(0.5, 0.5, 0.5)));
+    let debugon = std::rc::Rc::new(debugon::Debugon::new());
+
+    let top_sphere1 = std::rc::Rc::new(sphere::Sphere::new(vec3::Vec3::new(-TOP_SPHERE_RADIUS, 0.0, 0.0), TOP_SPHERE_RADIUS, metal.clone()));
+    let top_sphere2 = std::rc::Rc::new(sphere::Sphere::new(vec3::Vec3::new(TOP_SPHERE_RADIUS, 0.0, 0.0), TOP_SPHERE_RADIUS, debugon.clone()));
+    let bottom_sphere = std::rc::Rc::new(sphere::Sphere::new(vec3::Vec3::new(0.0, -100.0 - TOP_SPHERE_RADIUS, 0.0), 100.0, gray_lambertian.clone()));
 
     scene.add_object(top_sphere1.clone());
     scene.add_object(top_sphere2.clone());
@@ -26,6 +31,6 @@ fn main() {
         /*forward=*/vec3::Vec3::new(0.0, 0.0, -1.0),
     );
 
-    let im = camera.render(/*x_size=*/800, /*y_size=*/600, /*fov=*/90.0, /*bounces=*/10, /*samples_per_pixel=*/100);
+    let im = camera.render(/*x_size=*/800, /*y_size=*/600, /*fov=*/60.0, /*bounces=*/10, /*samples_per_pixel=*/100);
     println!("{}", im.to_ppm());
 }
